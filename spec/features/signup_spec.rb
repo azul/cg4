91d70afa77662fb16080b5880de2c52ebc6085ec
Azul <azul@riseup.net>
2014-03-05 09:01:17 +0100

    finally some real signup test.

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -7,9 +7,21 @@ describe 'Signup' do
       page.should have_content(I18n.t("sign_up"))
     end
 
+    it "signs up successfully" do
+      user = FactoryGirl.build :user
+      visit '/users/sign_up'
+      fill_in 'Name', with: user.name
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      fill_in 'Password confirmation', with:user.password
+      click_button I18n.t(:sign_up)
+      page.should have_content(I18n.t("devise.registrations.signed_up"))
+      user_record = User.find_by_email(user.email)
+      user_record.name.should eq(user.name)
+    end
+
     it 'reports missing fields' do
-      visit '/'
-      click_on I18n.t(:sign_up)
+      visit '/users/sign_up'
       click_button I18n.t(:sign_up)
       error_message = I18n.t "errors.messages.not_saved",
         resource: 'user',
