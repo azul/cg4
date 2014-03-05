22b9483561ac9be13c48aeddf2012f7b82244d93
Azul <azul@riseup.net>
2014-03-05 08:50:15 +0100

    fixup: better initial spec

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
 describe 'Signup' do
-  describe 'works' do
     it "displays signup form" do
       visit '/'
       click_on I18n.t(:sign_up)
+      page.should have_content(I18n.t("sign_up"))
     end
-  end
+
+    it 'reports missing fields' do
+      visit '/'
+      click_on I18n.t(:sign_up)
+      click_button I18n.t(:sign_up)
+      error_message = I18n.t "errors.messages.not_saved",
+        resource: 'user',
+        count: 2
+      expect(page).to have_content(error_message)
+    end
 end
