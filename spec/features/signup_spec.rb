0a503dd665285f5288f99b77a502558a84633f3e
Azul <azul@riseup.net>
2014-03-06 17:54:06 +0100

    Password is ambiguous as a field identifier
    Looks like capybara does not check the complete string but only the beginning. 'user_password' is a unique id.

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -12,7 +12,7 @@ describe 'Signup' do
       visit '/users/sign_up'
       fill_in 'Name', with: user.name
       fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
+      fill_in 'user_password', with: user.password
       fill_in 'Password confirmation', with:user.password
       click_button I18n.t(:sign_up)
       page.should have_content(I18n.t("devise.registrations.signed_up"))
