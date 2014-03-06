d7998af5f8bc0aea3034a1d0f6450f509e89ac2f
Azul <azul@riseup.net>
2014-03-06 22:21:37 +0100

    minor adopt signup spec to error message reality

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -23,9 +23,7 @@ describe 'Signup' do
     it 'reports missing fields' do
       visit '/users/sign_up'
       click_button I18n.t(:sign_up)
-      error_message = I18n.t "errors.messages.not_saved",
-        resource: 'user',
-        count: 2
-      expect(page).to have_content(error_message)
+      error_message = I18n.t "simple_form.error_notification.default_message"
+      page.should have_content(error_message)
     end
 end
