449241a0660e969b8d3763f7a14a4908d0f5893e
Azul <azul@riseup.net>
2014-03-09 21:27:16 +0100

    allow selecting group visibility during creation

--- spec/features/groups_spec.rb
+++ spec/features/groups_spec.rb
@@ -33,13 +33,23 @@ describe 'Groups' do
   end
 
   describe 'creation' do
+    let(:created_group) { Group.last }
     it 'can be reached from the landing page' do
       visit '/'
       click_on I18n.t("groups.nav")
       click_on I18n.t("groups.index.new")
       fill_in 'Name', with: 'Test Group'
       click_button 'Create group'
-      Group.last.name.should eq('Test Group')
+      created_group.name.should eq('Test Group')
+    end
+
+    it 'can create visible groups' do
+      visit '/groups/new'
+      fill_in 'Name', with: 'Visible Test Group'
+      select 'Visible to users', from: 'Visibility'
+      click_button 'Create group'
+      created_group.name.should eq('Visible Test Group')
+      created_group.visibility.should eq('visible_to_user')
     end
   end
 
